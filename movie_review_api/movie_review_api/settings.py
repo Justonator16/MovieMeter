99d242b12fde059794375490444b4469e9cacac7
Justonator16 <juniorjusto16@gmail.com>
2024-10-04 16:19:36 +0200

    Bug fix

--- movie_review_api/movie_review_api/settings.py
+++ movie_review_api/movie_review_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3lvaet%5*097e186yesvyh6rvq$ur=(j49fhx$@1wmv=c_htki'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'justo.pythonanywhere.com/movies/home/']
 
